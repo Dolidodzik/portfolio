a098527e25f29d8a9bb9d4792f13dd9872024059
Dolidodzik <teethtard321@gmail.com>
2020-03-06 08:26:08 +0000

    #4 Made technologies display only icons

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -8,27 +8,36 @@ export default class about extends Component {
       this.scroll_to_first_feature = React.createRef();
       this.scroll_to_top = React.createRef();
     }
-  HeaderClickEvent() {
-      this.scroll_to_first_feature.current.scrollIntoView({behavior: 'smooth'});
-    }
 
   render() {
 
-    function Feature(props){
+    function Technology(props){
       return(
-        <div className="Feature">
-          <img src={props.img_src} className="img-fluid feature-image col-6 col-md-3 " alt="" />
+        <div className="feature col-xl-2 col-lg-2 col-md-3 col-sm-4 col-6">
+          <img src={props.img_src} className="img-fluid" alt="" />
         </div>
       );
     }
 
     return (
-      <div className="component-about">
-        <Feature img_src={"fakeicon.png"} />
-        <Feature img_src={"fakeicon.png"} />
-        <Feature img_src={"fakeicon.png"} />
-        <Feature img_src={"fakeicon.png"} />
-        <Feature img_src={"fakeicon.png"} />
+      <div className="component-about container">
+        <div className="row">
+
+          <h1 className="col-12 about-header"> Technologies that I am familiar with: </h1>
+
+          <Technology img_src={"fakeicon.png"} />
+          <Technology img_src={"fakeicon.png"} />
+          <Technology img_src={"fakeicon.png"} />
+          <Technology img_src={"fakeicon.png"} />
+          <Technology img_src={"fakeicon.png"} />
+          <Technology img_src={"fakeicon.png"} />
+          <Technology img_src={"fakeicon.png"} />
+          <Technology img_src={"fakeicon.png"} />
+          <Technology img_src={"fakeicon.png"} />
+          <Technology img_src={"fakeicon.png"} />
+          <Technology img_src={"fakeicon.png"} />
+          <Technology img_src={"fakeicon.png"} />
+        </div>
       </div>
     );
 
