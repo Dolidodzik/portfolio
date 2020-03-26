d10a6eb4c1acf1095ab0a039505eb23c7d64f153
root <root@debianHOSTX.debianDOMAINX>
2020-03-26 11:22:20 +0000

    Updates

---
+++ precache-manifest.d95be1b3f9262a96c550ee2073da2a66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "077cdcdb4712ff078de5e4214f1156b2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "38131d80b55068210b65",
+    "url": "/portfolio/static/css/2.bc7dd8a7.chunk.css"
+  },
+  {
+    "revision": "6faa49b5c0e352a8ccb8",
+    "url": "/portfolio/static/css/main.7ef49617.chunk.css"
+  },
+  {
+    "revision": "38131d80b55068210b65",
+    "url": "/portfolio/static/js/2.bf3818da.chunk.js"
+  },
+  {
+    "revision": "6faa49b5c0e352a8ccb8",
+    "url": "/portfolio/static/js/main.37328c9d.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
