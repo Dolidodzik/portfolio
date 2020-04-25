47b454e7202e9a5e51f13e531106f3c283d2987e
root <root@debianHOSTX.debianDOMAINX>
2020-04-25 17:46:11 +0000

    Updates

---
+++ precache-manifest.18b7040e1a9ad1c2ab3c75010774339c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22e180a119d66afc69f713b5584331e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1267da042b136855201",
+    "url": "/static/css/2.ab7ddfd4.chunk.css"
+  },
+  {
+    "revision": "9015acb958cd7b206d5c",
+    "url": "/static/css/main.02e9434a.chunk.css"
+  },
+  {
+    "revision": "e1267da042b136855201",
+    "url": "/static/js/2.8d5376cd.chunk.js"
+  },
+  {
+    "revision": "9015acb958cd7b206d5c",
+    "url": "/static/js/main.e5a67b5e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
